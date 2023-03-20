c3bbba59567ee8c3b941ed2b7376f7ca871dec66
kimown <kimown@163.com>
2023-03-20 20:23:00 +0800

    feat:ok

--- app/headless_example2.cc
+++ app/headless_example2.cc
@@ -1,8 +1,27 @@
-#include <stdio.h>
 
+#include "libANGLE/Display.h"
 
 int main(int argc, char* argv[]) {
     printf("I am form headless_example2.cc\n");
+
+    {
+
+        EGLAttrib dispattrs[] = {EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_DEFAULT_ANGLE, EGL_NONE};
+        EGLDisplay mDisplay              = eglGetPlatformDisplay(EGL_PLATFORM_ANGLE_ANGLE,
+                                                      reinterpret_cast<void *>(EGL_DEFAULT_DISPLAY), dispattrs);
+
+
+        EGLint major;
+        EGLint minor;
+
+        EGLBoolean eglInitialized = eglInitialize(mDisplay, &major, &minor);
+
+        printf("%d %d %d\n", major, minor, eglInitialized);
+        printf("EGL vendor string: %s\n", eglQueryString(mDisplay, EGL_VENDOR));
+
+
+        return 0;
+    }
     return 0;
 }
 
