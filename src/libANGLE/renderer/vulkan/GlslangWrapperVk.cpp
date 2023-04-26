eed8a8b1fa64d00f0eea98947788dd56d3c7e1d9
kimown <kimown@163.com>
2023-04-26 19:06:37 +0800

    Update GlslangWrapperVk.cpp

--- src/libANGLE/renderer/vulkan/GlslangWrapperVk.cpp
+++ src/libANGLE/renderer/vulkan/GlslangWrapperVk.cpp
@@ -91,7 +91,7 @@ angle::Result GlslangWrapperVk::TransformSpirV(
     const SpirvBlob &initialSpirvBlob,
     SpirvBlob *shaderCodeOut)
 {
-    const bool removeDebugInfo = !context->getRenderer()->getEnableValidationLayers();
+    const bool removeDebugInfo = false;
 
     return GlslangTransformSpirvCode(
         [context](GlslangError error) { return ErrorHandler(context, error); }, shaderType,
