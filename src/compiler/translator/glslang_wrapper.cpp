d853405b0a3bb563b061cd0ad000a944fe86bd57
kimown <kimown@163.com>
2023-04-28 11:15:47 +0800

    feat:gVS

--- src/compiler/translator/glslang_wrapper.cpp
+++ src/compiler/translator/glslang_wrapper.cpp
@@ -156,13 +156,13 @@ void GlslangToSpv233(EShLanguage language, const char* shaderString, std::vector
 #ifdef _WIN32
         system("glslc  1111.vert -o 1111.vert.spv -g");
 #else
-        system("/media/google/Data/com/github/angle/tools/glslang/glslang_validator -V  1111.vert -o 1111.vert.spv -g");
+        system("/media/google/Data/com/mdk/1.3.243.0/x86_64/bin/glslangValidator -V  1111.vert -o 1111.vert.spv -gVS");
 #endif
     } else if(language == EShLangFragment){
 #ifdef _WIN32
         system("glslc  1111.frag -o 1111.frag.spv -g");
 #else
-        system("/media/google/Data/com/github/angle/tools/glslang/glslang_validator -V  1111.frag -o 1111.frag.spv -g");
+        system("/media/google/Data/com/mdk/1.3.243.0/x86_64/bin/glslangValidator -V  1111.frag -o 1111.frag.spv -gVS");
 #endif
     }
 
